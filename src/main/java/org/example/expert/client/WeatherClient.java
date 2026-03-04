c4626e19e1a2e307b05cf1629435c28628f0e47d
“grown-tree” <tjsruddks12@naver.com>
2026-03-04 17:57:03 +0900

    refactor : 리팩토링 퀴즈 : 불필요한 if-else 피하기해결
    throw로 else처리를 해주었으니 불필요한 depth제거

--- src/main/java/org/example/expert/client/WeatherClient.java
+++ src/main/java/org/example/expert/client/WeatherClient.java
@@ -27,13 +27,14 @@ public class WeatherClient {
                 restTemplate.getForEntity(buildWeatherApiUri(), WeatherDto[].class);
 
         WeatherDto[] weatherArray = responseEntity.getBody();
+        //throw로 else처리를 해주었으니 불필요한 depth제거
         if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
             throw new ServerException("날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
-        } else {
-            if (weatherArray == null || weatherArray.length == 0) {
-                throw new ServerException("날씨 데이터가 없습니다.");
-            }
         }
+        if (weatherArray == null || weatherArray.length == 0) {
+            throw new ServerException("날씨 데이터가 없습니다.");
+        }
+
 
         String today = getCurrentDate();
 
